5965a55c5ce52280e77c8aef55da16f24a0e35f2
Viktor Ivanov <vik.ivanov98@gmail.com>
2020-08-01 17:22:35 -0400

    Finished method and updated README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FolderTree 
+ This method print out a folder tree of the path passed in.
+
+ Format of the folder tree is:
+ ```
+- Root
+-  Subdirectory 1:
+   - file11
+   - file12
+   - Sub-sub-directory 11:
+         - file111
+         - file112
+- Subdirectory 2:
+    - file21
+    - sub-sub-directory 21
+    - sub-sub-directory 22    
+        - sub-sub-sub-directory 221
+            - file 2211
+```
--- rec_tree.py
+++ rec_tree.py
@@ -1,25 +1,5 @@
-'''
-Path:
-- Subdirectory 1:
-   - file11
-   - file12
-   - Sub-sub-directory 11:
-         - file111
-         - file112
-- Subdirectory 2:
-    - file21
-    - sub-sub-directory 21
-    - sub-sub-directory 22    
-        - sub-sub-sub-directory 221
-            - file 2211
-'''
 import os
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-#print('1 ' + os.getcwd())
-path = os.getcwd() + '/test'
-os.chdir(path)
-#print('2 ' + os.getcwd())
 
 def folder_tree(path):
     tree = os.walk(path)
@@ -30,5 +10,3 @@ def folder_tree(path):
         sindent = ' ' * 4 * (level + 1)
         for f in files:
             print(f'{sindent}{f}')
-
-folder_tree(path)
